63faeb7e2a4ebfd2040aa258e7a125a20e154dff
Rakotoarivao <aina.rakotoarivao@gmail.com>
2023-06-01 13:27:08 +0300

    add

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+// Classe représentant un employé
+public class Employe
+{
+    public string? Nom { get; set; }
+    public double SalaireDeBase { get; set; }
+    public bool EstTempsPlein { get; set; }
+
+    public double CalculerSalaire()
+    {
+        double salaire = SalaireDeBase;
+
+        // Calcul du salaire pour un employé à temps plein (+100 000 ariary de prime)
+        if (EstTempsPlein)
+        {
+            salaire += 10000;
+        }
+        // Calcul du salaire pour un employé à temps partiel (moitié)
+        else
+        {
+            salaire *= 0.5;
+        }
+
+        return salaire;
+    }
+}
+
+// Utilisation de la classe Employe
+public class Program
+{
+    static void Run()
+    {
+        Employe employe1 = new Employe
+        {
+            Nom = "Rakoto",
+            SalaireDeBase = 1000000,
+            EstTempsPlein = true
+        };
+
+        double salaireEmploye1 = employe1.CalculerSalaire();
+        Console.WriteLine($"Le salaire de {employe1.Nom} est de {salaireEmploye1} ariary.");
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+// Interface représentant un employé
+public interface IEmploye
+{
+    double CalculerSalaire();
+}
+
+// Classe de base pour les employés
+public abstract class EmployeBase : IEmploye
+{
+    public string? Nom { get; set; }
+    public double SalaireBase { get; set; }
+
+    public abstract double CalculerSalaire();
+}
+
+// Classe représentant un employé à temps plein
+public class EmployeTempsPlein : EmployeBase
+{
+    public override double CalculerSalaire()
+    {
+        // Calcul du salaire pour un employé à temps plein (+5 000 ariary de prime)
+        return SalaireBase + 5000;
+    }
+}
+
+// Classe représentant un employé à temps partiel
+public class EmployeTempsPartiel : EmployeBase
+{
+    public override double CalculerSalaire()
+    {
+        // Calcul du salaire pour un employé à temps partiel (moitié)
+        return SalaireBase * 0.5;
+    }
+}
+
+// Classe responsable du calcul du salaire pour tous les employés
+public class CalculateurSalaire
+{
+    private readonly IEmploye _employe;
+
+    public CalculateurSalaire(IEmploye employe)
+    {
+        _employe = employe;
+    }
+
+    public double CalculerSalaireTotal()
+    {
+        // Utilisation du principe Dependency Inversion Principle pour injecter l'employé à calculer
+        return _employe.CalculerSalaire();
+    }
+}
+
+// Utilisation du calculateur de salaire
+public class Programme
+{
+    static void AfficherSalaire(IEmploye employe)
+    {
+        double salaireEmploye = new CalculateurSalaire(employe).CalculerSalaireTotal();
+        Console.WriteLine($"Le salaire de {(employe as EmployeTempsPlein)?.Nom} est de {salaireEmploye} ariary.");
+    }
+
+    static void Main()
+    {
+        IEmploye employe1 = new EmployeTempsPlein
+        {
+            Nom = "Rakoto",
+            SalaireBase = 10000
+        };
+
+        IEmploye employe2 = new EmployeTempsPartiel
+        {
+            Nom = "Rabe",
+            SalaireBase = 15000
+        };
+
+        AfficherSalaire(employe1);
+        AfficherSalaire(employe2);
+    }
+}
